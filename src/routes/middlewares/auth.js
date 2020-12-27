e9845d1ee4ae7faf7e8805ac8c931a364fce44d8
sunilkandpal1o1 <sskandpal.uk@gmail.com>
2020-12-27 12:38:34 +0530

    Update to auth.js

--- src/routes/middlewares/auth.js
+++ src/routes/middlewares/auth.js
@@ -1,15 +1,21 @@
 import jwt from 'jsonwebtoken';
 
 const auth = async (req, res, next) => {
-  console.log('header ', req.header('Authorization').split('Bearer'));
-  let token = req.header('Authorization').split('Bearer')[1];
-  // const author_email = req.query.email;
-  const verify = await jwt.verify(token, process.env.JWT_SECRET);
-
-  if (verify) {
-    next();
+  console.log('header ', req.header('Authorization'));
+  if (!req.header('Authorization')) {
+    console.log('no auth header');
+    res.json({ err: 1, msg: 'You are not logged in' });
   } else {
-    res.status(401).json({ err: 1, msg: 'You are not logged in' });
+    console.log('header ', req.header('Authorization').split('Bearer'));
+    let token = req.header('Authorization').split('Bearer')[1];
+    // const author_email = req.query.email;
+    const verify = await jwt.verify(token, process.env.JWT_SECRET);
+
+    if (verify) {
+      next();
+    } else {
+      res.status(401).json({ err: 1, msg: 'Session expired' });
+    }
   }
 };
 
